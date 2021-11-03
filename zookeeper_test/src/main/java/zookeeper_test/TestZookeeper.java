ddf8d59bfffe1d379affae8284622bcd2aca744f
Reggielang <37894298+Reggielang@users.noreply.github.com>
2021-11-03 20:33:54 +0800

    Add files via upload

--- zookeeper_test/src/main/java/zookeeper_test/TestZookeeper.java
+++ zookeeper_test/src/main/java/zookeeper_test/TestZookeeper.java
@@ -1,11 +1,13 @@
 package zookeeper_test;
 
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class TestZookeeper {
     //zookeeper客户端访问集群的端口号
@@ -18,12 +20,23 @@ public class TestZookeeper {
          zkClient = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
             @Override
             public void process(WatchedEvent watchedEvent) {
-
+                List<String> children = null;
+                try {
+                    children = zkClient.getChildren("/", true);
+                    for (String child : children) {
+                        System.out.println(child);
+                    }
+                    System.out.println("-------------------");
+                } catch (KeeperException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
 
-    //创建子节点
+    //1.创建子节点
     @Test
     public void createNode() throws KeeperException, InterruptedException {
         String path = zkClient.create("/honelang", "wozuishuai".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
@@ -31,6 +44,26 @@ public class TestZookeeper {
 
 
     }
+    //2.获取子节点，并监控数据的变化
+    @Test
+    public void getDataAndWatch() throws KeeperException, InterruptedException {
+        List<String> children = zkClient.getChildren("/", true);
+
+        for (String child : children) {
+            System.out.println(child);
+        }
+        Thread.sleep(Long.MAX_VALUE);
+    }
+
+    //3.判断节点是否存在
+    @Test
+    public void exist() throws KeeperException, InterruptedException {
+        Stat stat = zkClient.exists("/", false);
+
+        System.out.println(stat==null?"not exist":"exist");
+    }
+
+
 
 
 }
